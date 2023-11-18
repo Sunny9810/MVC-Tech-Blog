251dec24bb00d4f4d22893acdce97e8b20e7ff24
Arevik-Sunny <arevik.abrahamyan18@gmail.com>
2023-11-18 00:03:58 -0800

    adding image

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config(); // Import sensitive data from .env
 
 const sequelize = process.env.JAWSDB_URL 
-  ? new Sequelize(process.env.JAWSDB_URL) // JAWSDB_URL is for HEROKU
+  ? new Sequelize(process.env.JAWSDB_URL) 
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // This is for Local
       host: '127.0.0.1',
       dialect: 'mysql',
